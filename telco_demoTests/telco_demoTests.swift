8026eaec2b20f4decda7efaf8c9c89c2ecbff95c
Axel Kee <axelshaded@gmail.com>
2017-02-15 16:47:30 +0800

    Initial Commit

---
+++ telco_demoTests/telco_demoTests.swift
@@ -0,0 +1,36 @@
+//
+//  telco_demoTests.swift
+//  telco_demoTests
+//
+//  Created by Axel Kee on 15/02/2017.
+//  Copyright © 2017 Sweatshop. All rights reserved.
+//
+
+import XCTest
+@testable import telco_demo
+
+class telco_demoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
